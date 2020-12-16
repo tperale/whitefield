cc9d07543d82c1cbb9103da0b0de0221a5cba4a4
Thomas Perale <perale.thomas@gmail.com>
2020-12-16 11:24:21 +0100

    linting invoke_whitefield

--- invoke_whitefield.sh
+++ invoke_whitefield.sh
@@ -11,9 +11,9 @@ if [ "$1" == "gdb" ]; then
 fi
 
 [[ ! -f "$1" ]] && echo "Usage: $0 <wf_config_file>" && exit
-cfgfile=`pwd`"/$1"
+cfgfile=$(pwd)"/$1"
 
-cd `dirname $0`
+cd $(dirname $0)
 [[ ! -f "config.inc" ]] && echo "Need to start whitefield from base folder!!" && exit 1
 . config.inc
 
@@ -26,36 +26,33 @@ export AIRLINE_LOG=$LOGPATH/airline.log
 #export NS_LOG="*=level_warn:LrWpanMac=level_all"
 rm -f $LOGPATH/*.log 2>/dev/null
 
-diagnose()
-{
-    grep "^ERROR" $AIRLINE_LOG
-    echo -e "${BLINK}Start Failed.${NC}"
-    echo "1. Check logs in folder:$LOGPATH."
-    echo "2. Enable and check coredumps."
-    exit 1
+diagnose() {
+	grep "^ERROR" $AIRLINE_LOG
+	echo -e "${BLINK}Start Failed.${NC}"
+	echo "1. Check logs in folder:$LOGPATH."
+	echo "2. Enable and check coredumps."
+	exit 1
 }
 
-func_childret()
-{
-    kill -0 $wf_ps 2>/dev/null
-    [[ $? -ne 0 ]] && diagnose
+func_childret() {
+	kill -0 $wf_ps 2>/dev/null
+	[[ $? -ne 0 ]] && diagnose
 	jobs -l | grep "Running" >/dev/null
-    echo "Started OK"
-    echo "Use './scripts/monitor.sh' to check status."
+	echo "Started OK"
+	echo "Use './scripts/monitor.sh' to check status."
 	exit $?
 }
 
-wfpid=$(pgrep -u `whoami` -x whitefield)
+wfpid=$(pgrep -u $(whoami) -x whitefield)
 [[ $? -eq 0 ]] && echo "Whitefield(pid=$wfpid) is already in execution!" && exit 1
-
 mkdir $LOGPATH pcap 2>/dev/null
 if [ "$cmdprefix" == "" ]; then #Regular execution
 	trap func_childret SIGCHLD
 	set -m
 	$BINDIR/whitefield $* >$AIRLINE_LOG 2>&1 &
-    wf_ps=$!
+	wf_ps=$!
 	sleep 1
-	echo ;
+	echo
 else #GDB execution
 	$cmdprefix $BINDIR/whitefield $*
 fi
